64a636c00f51689a405bb18e541d7a37ce45fd9a
einartornes <45436606+einartornes@users.noreply.github.com>
2022-05-09 21:06:02 +0200

    update datasource

--- import_data.R
+++ import_data.R
@@ -10,7 +10,7 @@ library(noradstats)
 
 # Kilde A: Bistandsstatistikk 10 år (oda_ten)
 df_oda_ten <- 
-  noradstats::read_aiddata("data/statsys_ten.csv") |>
+  noradstats::read_aiddata("data", "statsys_ten.csv") |>
   filter(`Type of Flow` == "ODA") |>
   filter(`Type of agreement` != "Rammeavtale") |>
   filter(Year %in% max(Year-9):max(Year)) |> 
