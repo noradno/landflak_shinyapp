42a92de4a353c88db4e7935d6a1a71cca3bd0179
einartornes <45436606+einartornes@users.noreply.github.com>
2023-01-02 22:28:28 +0100

    updating years

--- get_data_donors.R
+++ get_data_donors.R
@@ -5,7 +5,7 @@ library(tidyverse)
 library(here)
 
 # Velger år
-vec_year <- 2020
+vec_year <- 2021
 
 # Her brukes OECD-datasettet TABLE2A inkludert metadata.
 # For å spesifisere key-argumentet, så identifiser hvilke keys-dimensjoner som finnes i datasettet og kan spesifiseres. Punktum-tegnet skiller dimensjonene.
